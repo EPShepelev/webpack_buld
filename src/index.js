5983e6fb5f95d63609623041376754cb7271c614
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2020-04-17 14:35:34 +0300

    install loaders: file,  style and  css, added require rules to webpack-config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import Post from './post'
+import './styles/style.css'
+import json from './assets/file1.json'
+import Smile from './assets/smile.png'
 
-const post = new Post('Post title 1')
+const post = new Post('Post title 1', Smile)
 
 console.log(post.toString())
+
+console.log('JSON-file:', json)
