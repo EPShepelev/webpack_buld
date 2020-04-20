850c7ede765667c0a429e5bb44fed8c45dab3214
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2020-04-20 20:22:11 +0300

    install xml, csv loaders; added rules; install and import normalize-css

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import Post from './post'
 import './styles/style.css'
 import json from './assets/file1.json'
 import Smile from './assets/smile.png'
+import xml from './assets/file2.xml'
+import csv from './assets/file3.csv'
 
 const post = new Post('Post title 1', Smile)
 
 console.log(post.toString())
 
 console.log('JSON-file:', json)
+console.log('XML-file:', xml)
+console.log('CSV-file:', csv)
