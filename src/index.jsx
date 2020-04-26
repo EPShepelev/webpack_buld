66f95158e409f3a6a247456ef37758d713a392a0
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2020-04-26 12:52:26 +0300

    added react and index.js was changed to index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,41 @@
+import * as $ from 'jquery'
+import Post from '@models/post'
+import './styles/style.css'
+import './styles/less.less'
+import './styles/scss.scss'
+import json from './assets/file1.json'
+import Smile from '@/assets/smile.png'
+import xml from './assets/file2.xml'
+import csv from './assets/file3.csv'
+import './babel.js'
+import React from 'react'
+import {render} from 'react-dom'
+
+const post = new Post('Post title 1', Smile)
+
+// $('pre').html(post.toString())
+
+// console.log(post.toString())
+
+console.log('JSON-file:', json)
+console.log('XML-file:', xml)
+console.log('CSV-file:', csv)
+
+const App = () => (<div class='container'>
+  <h1>Easy webpack build for training</h1>
+  <hr/>
+  <div class='logo'></div>
+  <hr/>
+  <pre/>
+  <hr/>
+  <div class='box'>
+  <h2>Styles set with less</h2>
+  </div>
+  <hr/>
+  <div class='card'>
+  <h2>Styles set with sass</h2>
+  </div>
+  </div>
+  )
+
+render(<App/>, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -61,7 +61,7 @@ module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
   entry: {
-    main: ['@babel/polyfill','./index.js'],
+    main: ['@babel/polyfill','./index.jsx'],
     analytics: './analytics.ts'
   },
   output: {
